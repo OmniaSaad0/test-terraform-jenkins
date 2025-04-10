409ad69321d18a056b2b8d711b51514f1a1e8d6a
OmniaSaad0 <saadomnia509@gmail.com>
2025-04-10 16:07:37 +0200

    modify terraform backend

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-states3-bucket"
-    key          = "terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
